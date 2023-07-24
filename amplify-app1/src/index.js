87e9a9f5e298f002924b4e02819a06bea9ee9ec4
VuLinh <vulinhvovan@gmail.com>
2023-07-25 00:22:06 +0700

    update

--- amplify-app1/src/index.js
+++ amplify-app1/src/index.js
@@ -4,11 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import "@aws-amplify/ui-react/styles.css"; // Ensure React UI libraries are styled correctly
-import { Amplify } from 'aws-amplify'
-import awsconfig from './aws-exports'
-Amplify.configure(awsconfig) // Configures the Amplify libraries with the cloud backend set up via the Amplify CLI
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
